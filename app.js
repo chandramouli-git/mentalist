6d18f73a05a6a5b339c190f978d7029a7541edad
chandramouli-git <60184634+chandramouli-git@users.noreply.github.com>
2021-09-14 09:00:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var express= require('express');
+var app=express();
+app.set('view engine','ejs');
+app.use('/asset',express.static(__dirname+'/js'));
+app.get('/',function(req,res)
+{
+    res.render('Welcome');
+});
+app.listen(3030);
+
+
